d2b083efca0181b25de70cdc2cec417577043bb7
Selihom Efrem Ogbe <85318950+seli-hom@users.noreply.github.com>
2025-04-05 16:55:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.get("/api/excuse", async (req, res) =>{
   
 });
 
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(3000, () => {
-  console.log(`Server running on port 3000`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 // app.get('/api/excuse', async (req, res) => {
